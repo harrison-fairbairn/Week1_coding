debd7db064b7688e9bb7d389861fab17ee0e12cc
harrison-fairbairn <71247590+harrison-fairbairn@users.noreply.github.com>
2020-09-19 22:07:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Questions 1-4
+
 var jonItemPrice = 30;
 var kateItemPrice = 45;
 
@@ -37,4 +39,40 @@ console.log('Jon has this much money:' +500)
 
 let kateAmountMoney = "300";
 console.log('Kate has this much money:' +300)
+
+let jonItemPrice = "30";
+console.log('Jon paid this much for the item:' +30)
+
+let kateItemPrice = "45";
+console.log('Kate paid this much for the item:' +45)
+
+let jonNumberFriends = "10";
+console.log('Jon has this many friends:' +10)
+
+let kateNumberFriends = "15";
+console.log('Kate has this many friends:' +15)
+
+let jonNumberYears = "25";
+console.log('Jon is this many years old:' +25)
+
+let kateNumberYears = "18";
+console.log('Kate is this many years old:' +18)
+
+let jonFirstName = "Jon";
+console.log('Jons first name is:' +Jon)
+
+let kateFirstName = "Kate";
+console.log('Kates first name is:' +Kate)
+
+let jonLastName = "Fairbairn";
+console.log('Jons last name is:' +Fairbairn)
+
+let kateLastName = "Fairbairn";
+console.log('Kates last name is:' +Fairbairn)
+
+let jonMiddleInitial = "M";
+console.log('Jons middle initial is:' +M)
+
+let kateMiddleInitial = "E";
+console.log('Kates middle initial is:' +E)
 
