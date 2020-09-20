b3135fce27e2210c848dcc6154e35d9390112605
Harrison <harrisonchasefairbairn@gmail.com>
2020-09-19 21:17:19 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var jonItemPrice = 30;
+var kateItemPrice = 45;
+
+var jonAmountMoney = 500;
+var kateAmountMoney = 300;
+
+var jonNumberFriends = 10;
+var kateNumberFriends = 15;
+
+var jonNumberYears = 25;
+var kateNumberYears = 18;
+
+var jonFirstName = "JON"
+var kateFirstName = "KATE"
+
+var jonLastName = "FAIRBAIRN"
+var kateLastName = "FAIRBAIRN"
+
+var jonMiddleInitial = "M"
+var kateMiddleInitial = "E" 
+
+// Moving on to question 5
+
+var jonNewMoney = jonAmountMoney - jonItemPrice; 
+var kateNewMoney = kateAmountMoney - kateItemPrice; 
+
+var jfriendsPerYear = jonNumberYears / jonNumberFriends;
+var kfriendsPerYear = kateNumberYears / kateNumberFriends;
+
+var jonFullName = jonFirstName + jonMiddleInitial + jonLastName;
+var kateFullName = kateFirstName + kateMiddleInitial + kateLastName;
+
+if (10 > 5) { 
+    console.log('ten is less than five');
+}  else { 
+       console.log('ten is not less than five');
+}
+if ( 10 > 5 )  {
+
+} else
+
+// Probably not fully getting this yet
+
+
+ (var i = 0; i < 10; i++) {
+
+  if (i  == 3 || i == 6) {
+
+    i++;
+
+  }
+
+  console.log(i);
+
+}if (10 > 5)
